89ccc0b01e9c01acb8749179df783872c361d892
Vinicius Santana Neves <viniciussantanadesign@gmail>
2024-04-17 19:35:10 -0300

    alteaçoes

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
+import { sql } from "@vercel/postgres";
 
 export default function Page() {
   return (
